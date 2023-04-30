abce4490fc103ab2c0ed3f3128394b966f0c222e
Martin Carroll <mcar0037@student.monash.edu>
2023-04-30 22:42:30 +1000

    gestures page for spike

--- flutter_spike/lib/gestures.dart
+++ flutter_spike/lib/gestures.dart
@@ -1,14 +1,46 @@
 import 'package:flutter/material.dart';
 
-class GesturesPage extends StatelessWidget {
+class GesturesPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return GesturesState();
+  }
+}
+
+class GesturesState extends State<GesturesPage> {
+  void doubleTap() => {
+        setState(() => {gestureText = "Double tap"})
+      };
+  void longPress() => {
+        setState(() => {gestureText = "Long press"})
+      };
+  void verticalSwipe(details) => {
+        setState(() => {gestureText = "Vertical swipe"})
+      };
+  void horizontalSwipe(details) => {
+        setState(() => {gestureText = "Horizontal swipe"})
+      };
+  String gestureText = 'No gestures yet';
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: Text("Return home")),
+    return GestureDetector(
+      onDoubleTap: doubleTap,
+      onLongPress: longPress,
+      onVerticalDragEnd: verticalSwipe,
+      onHorizontalDragEnd: horizontalSwipe,
+      child: Scaffold(
+          appBar: AppBar(title: Text("Gesture page")),
+          body: Center(
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text("Return home")),
+                Text(gestureText)
+              ]))),
     );
   }
 }
