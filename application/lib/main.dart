dd9097cdefff98efc6c584c6b94c7f3544585008
haotongwang <nicholas.wang.h@gmail.com>
2023-05-05 12:08:42 +1000

    Add routes

--- application/lib/main.dart
+++ application/lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:application/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:application/tutorial/tutorial_main.dart';
+import 'package:application/tutorial/two/tutorial_two.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,21 +14,26 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Spike',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Spike'),
-    );
+        title: 'Flutter Spike',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.blue,
+        ),
+        initialRoute: Routes.home,
+        routes: {
+          Routes.home: (context) =>
+              const MyHomePage(title: 'Flutter Demo Home Page'),
+          Routes.tutorials: (context) => const TutorialMain(),
+          Routes.tutorialTwo: (context) => const TutorialTwo(),
+        });
   }
 }
 
@@ -133,6 +140,7 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ],
         onTap: ((index) {
+          String path = Routes.home;
           switch (index) {
             case 0:
               //home
@@ -142,13 +150,10 @@ class _MyHomePageState extends State<MyHomePage> {
               break;
             case 2:
               // tutorials
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const TutorialMain()),
-              );
+              path = Routes.tutorialTwo;
               break;
-            default:
           }
+          Navigator.pushNamed(context, path);
         }),
       ),
     );
