226de4fb05eef15a98fcb897424590c7413b3497
haotongwang <nicholas.wang.h@gmail.com>
2023-05-12 00:07:08 +1000

    Use tts for go back module rather than announce
    Keep empty announce to interrupt any ongoing announcements

--- application/lib/tutorial/two/module/go_back.dart
+++ application/lib/tutorial/two/module/go_back.dart
@@ -6,14 +6,26 @@ class GoBack extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    SemanticsService.announce(
-        "To go back, swipe down then left with one finger in a continuous action. If multi-gestures are enabled, swipe with two fingers from the left or right side of the screen.",
-        TextDirection.ltr);
+    SemanticsService.announce("", TextDirection.ltr);
 
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false, // Disable back button
         title: const Text("Go Back Module"),
+      ),
+      body: Card(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: const <Widget>[
+            ListTile(
+              title: Focus(
+                autofocus: true,
+                child: Text(
+                    'To go back, swipe down then left with one finger in a continuous action. If multi-gestures are enabled, swipe with two fingers from the left or right side of the screen.'),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
