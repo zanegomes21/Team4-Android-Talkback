5d570be8f208be0bafc1b1f32d41d8815e05a04a
PaarthSh4rma <paarth.sharma.3110@gmail.com>
2023-05-13 14:54:30 +1000

    VoiceRecorder App Module initialisation

---
+++ tutorial7/lib/voice_recorder_app_module.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+//import Voice Recorder Actual Activity;
+import 'module.dart';
+
+class VoiceRecorderAppModule extends Module{
+  @override
+  late final String title = "Voice Recorder App";
+
+  @override
+  void startScreen(BuildContext context) {
+    // TODO: implement startScreen
+  }
+}
