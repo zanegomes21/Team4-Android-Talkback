8e047844df870530b8e0d5aa0f4ed07b478a5f73
Shreshth Saharan <ssah0022@student.monash.edu>
2023-05-15 14:48:04 +1000

    Have initialised the video player controller. Did this by following along the tutorial on Flutter's official documentation.

--- application/lib/tutorial/four/module/start_stop_media.dart
+++ application/lib/tutorial/four/module/start_stop_media.dart
@@ -11,4 +11,57 @@ class StartStopMedia extends StatefulWidget {
 class _StartStopMediaState extends State<StartStopMedia> {
   late VideoPlayerController _controller;
   late Future<void> _initialiseVideoPlayerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = VideoPlayerController.network(
+        'https://www.youtube.com/watch?v=jNQXAC9IVRw');
+    _initialiseVideoPlayerFuture = _controller.initialize();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Me at the zoo'),
+      ),
+      body: FutureBuilder(
+        future: _initialiseVideoPlayerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return AspectRatio(
+              aspectRatio: _controller.value.aspectRatio,
+              child: VideoPlayer(_controller),
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            if (_controller.value.isPlaying) {
+              _controller.pause();
+            } else {
+              _controller.play();
+            }
+          });
+        },
+        child: Icon(
+          _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+        ),
+      ),
+    );
+  }
 }
