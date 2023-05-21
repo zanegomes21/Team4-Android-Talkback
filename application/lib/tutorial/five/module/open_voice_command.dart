19ed47787ab79f193bfadb30c4ea2c0014f5a42e
Zane Gomes <zgom0003@student.monash.edu>
2023-05-21 23:09:55 +1000

    -Added basic functionality for open_voice_command.dart from tutorial 5. -pubspec.lock has the flutter_tts dependency added to it

--- application/lib/tutorial/five/module/open_voice_command.dart
+++ application/lib/tutorial/five/module/open_voice_command.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/semantics.dart';
+
 import 'package:flutter_tts/flutter_tts.dart';
-import 'package:speech_to_text/speech_recognition_result.dart';
-import 'package:speech_to_text/speech_to_text.dart';
 
 class OpenVoiceCommand extends StatefulWidget {
   const OpenVoiceCommand({Key? key}) : super(key: key);
@@ -11,7 +9,8 @@ class OpenVoiceCommand extends StatefulWidget {
   State<OpenVoiceCommand> createState() => _OpenVoiceCommand();
 }
 
-class _OpenVoiceCommand extends State<OpenVoiceCommand> {
+class _OpenVoiceCommand extends State<OpenVoiceCommand>
+    with WidgetsBindingObserver {
   String introMessage = """
     Open Voice Commands using gestures.
     Welcome, in this module you will learn to enabel voice commands.
@@ -22,50 +21,97 @@ class _OpenVoiceCommand extends State<OpenVoiceCommand> {
     Good luck!
     """;
 
+  String instructionMessage = """
+    Well Done!
+    Next we will put some voice commands into action.
+    Now that you know some voice commands, we will show you how to increase Talk
+    Backs reading speed using voice command.
+    """;
+
+  String fasterSpeechTitle = "Swipe right.";
+
+  String fasterSpeechOne = """
+    To make me speak faster swipe right then up with one finger to open voice command
+    gain. Do this gesture, then say speak faster. And move to the next element when 
+    you are done. Good luck!
+    """;
+
+  String fasterSpeechTwo = """
+    I am now speaking faster. When you have compleated listening to this text go 
+    to the next element and select the button to finish the module.
+    """;
+
+  bool isIntro = true;
+  bool isFasterSpee = false;
+  FlutterTts? flutterTts;
+
   @override
   void initState() {
     super.initState();
+    WidgetsBinding.instance.addObserver(this);
 
     // Initilizing tts engine
-    _initTextToSpeech().then((flutterTts) {
-      _speakLines(flutterTts, introMessage);
+    _initTextToSpeech().then((value) {
+      flutterTts = value;
+      _speakLines(flutterTts!, introMessage);
     });
-
   }
 
   Future<FlutterTts> _initTextToSpeech() async {
+    String lang = 'en-US';
+    double narrationSpeed = 0.45;
+
     FlutterTts flutterTts = FlutterTts();
-    flutterTts.setLanguage('en-US');
-    flutterTts.setSpeechRate(0.45);
+    flutterTts.setLanguage(lang);
+    flutterTts.setSpeechRate(narrationSpeed);
     return flutterTts;
   }
 
   void _speakLines(FlutterTts flutterTts, String message) async {
+    String line =
+        message.replaceAll('\n', ' '); // Multi line str into str array
 
-    List<String> lines = message.split('\n');
-    for (String line in lines) {
-      if (line.trim().isNotEmpty) {
-        await flutterTts.awaitSpeakCompletion(true);  // Wait for tts occouring prior to finish
-        await flutterTts.speak(line.trim());          // Read line form messager after prior tts
-      }
+    flutterTts.speak(line);
+  }
+
+  void _resetTts() async {
+    await flutterTts!.stop();
+    flutterTts = null;
+  }
+
+  @override
+  void dispose() {
+    flutterTts!.stop();
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    super.didChangeAppLifecycleState(state);
+
+    if (state != AppLifecycleState.resumed) {
+      return;
     }
-    await flutterTts.awaitSpeakCompletion(true);      // Wait for last tts to complete
+
+    changeStage();
+  }
+
+  void changeStage() {
+    if (isIntro) {
+      setState(() {
+        isIntro = false;
+        isFasterSpee = true;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-
     return Scaffold(
       // Title
       appBar: AppBar(
-        automaticallyImplyLeading: false, // Disable back button
-        title: const Text("open_voice_command"),
+        title: const Text("Open Voice Commands"),
       ),
 
       // Body
@@ -78,24 +124,96 @@ class _OpenVoiceCommand extends State<OpenVoiceCommand> {
 
           // All the elements of the page
           children: [
-
-            // Intor of module
-            FutureBuilder<FlutterTts>(
-                future: _initTextToSpeech(),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.done) {
-                    if (snapshot.hasData) {
-                      _speakLines(snapshot.data!, introMessage);
+            if (isIntro)
+              // This speaks the intro
+              FutureBuilder<FlutterTts>(
+                  future: _initTextToSpeech(),
+                  builder: (context, snapshot) {
+                    // This reades instructions and checks for successfull
+                    // execution of said instructions.
+                    if (snapshot.connectionState == ConnectionState.done) {
+                      if (snapshot.hasData) {
+                        _speakLines(snapshot.data!, introMessage);
+                      }
+                      return const SizedBox();
+                    } else {
+                      return const CircularProgressIndicator();
                     }
-                    return const SizedBox.shrink();
-                  } else {
-                    return const CircularProgressIndicator();
-                  }
-                })
-
+                  }),
+            if (isIntro)
+              // This is the instructions for the above intro
+              const Text(
+                'Swipe right then up\nTo open voice commands',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+            if (isFasterSpee)
+              // This speaks the intro
+              FutureBuilder<FlutterTts>(
+                  future: _initTextToSpeech(),
+                  builder: (context, snapshot) {
+                    // This reades instructions and checks for successfull
+                    // execution of said instructions.
+                    if (snapshot.connectionState == ConnectionState.done) {
+                      if (snapshot.hasData) {
+                        _speakLines(snapshot.data!, instructionMessage);
+                      }
+                      return const SizedBox();
+                    } else {
+                      return const CircularProgressIndicator();
+                    }
+                  }),
+            if (isFasterSpee) Text(fasterSpeechTitle),
+            if (isFasterSpee) Text(fasterSpeechOne),
+            if (isFasterSpee) Text(fasterSpeechTwo)
           ],
         ),
       ),
     );
   }
 }
+
+/*
+child: FutureBuilder<FlutterTts>(
+                  future: _initTextToSpeech(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.done) {
+                      if (snapshot.hasData) {
+                        _speakLines(snapshot.data!, introMessage);
+                      }
+                    } else {
+                      return const CircularProgressIndicator();
+                    }
+                    return Container(); // Return a placeholder widget here if needed
+                  },
+                ),
+                */
+
+
+// Intor of module
+            /*
+              FutureBuilder<FlutterTts>(
+                future: _initTextToSpeech(),
+                builder: (context, snapshot) {
+                  // This reades instructions and checks for successfull
+                  // execution of said instructions.
+                  if (snapshot.connectionState == ConnectionState.done) {
+                    if (snapshot.hasData) {
+                      _speakLines(snapshot.data!, introMessage);
+                    }
+
+                    return Expanded(
+                        child: GestureDetector(
+                            behavior: HitTestBehavior.translucent,
+                            onPanUpdate: (details) {
+                              if (details.delta.dx > 5 &&
+                                  details.delta.dy < 5) {
+                                _openVoiceCmdSuccess();
+                              }
+                            },
+                            child: Container()));
+                  } else {
+                    return const CircularProgressIndicator();
+                  }
+                }),
+                  */
