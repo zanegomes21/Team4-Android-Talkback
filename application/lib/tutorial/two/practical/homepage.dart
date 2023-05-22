2b1a1bcab1a28675b30a799e7ec08612daa967c6
haotongwang <nicholas.wang.h@gmail.com>
2023-05-22 14:54:26 +1000

    Add shop items
    Clicking on an item opens it in a window

--- application/lib/tutorial/two/practical/homepage.dart
+++ application/lib/tutorial/two/practical/homepage.dart
@@ -1,6 +1,9 @@
 import 'package:application/tutorial/two/practical/pricerangeslider.dart';
 import 'package:flutter/material.dart';
 
+import 'item.dart';
+import 'data_item.dart' show data;
+
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -37,6 +40,55 @@ class HomePage extends StatelessWidget {
               ),
             ),
             PriceRangeSlider(onRangeUpdate: onRangeUpdate),
+            const Padding(
+              padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
+              child: Text(
+                "Items",
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Expanded(
+              child: ListView(
+                children: data
+                    .where((d) => d.price >= 0 && d.price <= 10000)
+                    .map(
+                      (d) => GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => Scaffold(
+                                appBar: AppBar(title: Text(d.name)),
+                                body: Item.fromData(data: d),
+                              ),
+                            ),
+                          );
+                        },
+                        child: Card(
+                          key: Key(d.id.toString()),
+                          child: Column(
+                            children: <Widget>[
+                              ListTile(
+                                title: Text(d.name),
+                                subtitle: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text("Rating: ${d.rating}"),
+                                    Text("Price: ${d.price / 100}"),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    )
+                    .toList(),
+              ),
+            ),
           ],
         ),
       ),
