d07a54043be02a7be55259ba629ef0cf50116b4d
haotongwang <nicholas.wang.h@gmail.com>
2023-05-22 14:54:26 +1000

    Add shop items
    Clicking on an item opens it in a window

---
+++ application/lib/tutorial/two/practical/data_item.dart
@@ -0,0 +1,96 @@
+class ItemData {
+  final int id;
+  final String name;
+  final double rating;
+  final int price;
+  final String description;
+
+  ItemData(
+      {required this.id,
+      required this.name,
+      required this.rating,
+      required this.price,
+      required this.description});
+
+  factory ItemData.fromJson(Map<String, dynamic> json) {
+    return ItemData(
+        id: json['id'],
+        name: json['name'],
+        rating: json['rating'],
+        price: json['price'],
+        description: json['description']);
+  }
+}
+
+var data = [
+  {
+    "id": 0,
+    "name": "Tennis Ball",
+    "rating": 3.0,
+    "price": 200,
+    "description":
+        "This extra duty tennis ball is ideal for longer play on the courts. Perfect for all levels of tournament and recreational play.",
+  },
+  {
+    "id": 1,
+    "name": "Basketball",
+    "rating": 4.5,
+    "price": 300,
+    "description":
+        "This basketball is designed for indoor or outdoor play. It features a deep channel design for easier hand alignment and ball control.",
+  },
+  {
+    "id": 2,
+    "name": "Soccer Ball",
+    "rating": 4.0,
+    "price": 1000,
+    "description": "This soccer ball is designed for indoor or outdoor play.",
+  },
+  {
+    "id": 3,
+    "name": "Baseball",
+    "rating": 4.5,
+    "price": 500,
+    "description": "This baseball has a cork core and a leather cover.",
+  },
+  {
+    "id": 4,
+    "name": "Football",
+    "rating": 4.0,
+    "price": 1000,
+    "description":
+        "This football is ideal for recreational play or practice. It features a leather cover for durability and a deep pebble surface for grip.",
+  },
+  {
+    "id": 5,
+    "name": "Train Set",
+    "rating": 4.0,
+    "price": 10000,
+    "description":
+        "This train set is ideal for children. It features a train, tracks, and a station.",
+  },
+  {
+    "id": 6,
+    "name": "Doll",
+    "rating": 4.0,
+    "price": 2000,
+    "description":
+        "This doll is ideal for children. It can be dressed up in different outfits. Outfits sold separately.",
+  },
+  {
+    "id": 7,
+    "name": "Unicorn Plush",
+    "rating": 4.0,
+    "price": 8000,
+    "description":
+        "This unicorn plush is ideal for children. It is soft and cuddly.",
+  },
+  {
+    "id": 8,
+    "name": "Plastic Castle",
+    "rating": 4.0,
+    "price": 5000,
+    "description":
+        "Contains multiple pieces to build a castle including walls, towers, and a gate. For children ages 3 and up. Choking hazard.",
+  },
+].map(ItemData.fromJson).toList();
---
+++ application/lib/tutorial/two/practical/item.dart
@@ -0,0 +1,72 @@
+import 'dart:ui';
+
+import 'package:application/tutorial/two/practical/data_item.dart';
+import 'package:flutter/material.dart';
+
+class Item extends StatelessWidget {
+  final String name;
+  final int price;
+  final double? rating;
+  final String description;
+
+  const Item({
+    Key? key,
+    required this.name,
+    required this.price,
+    required this.description,
+    this.rating,
+  }) : super(key: key);
+
+  Item.fromData({
+    Key? key,
+    required ItemData data,
+  }) : this(
+          key: key,
+          name: data.name,
+          price: data.price,
+          description: data.description,
+          rating: data.rating,
+        );
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            name,
+            style: const TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(10, 10, 0, 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Price: \$${price / 100}"),
+                Text("Rating: ${rating ?? 'N/A'}"),
+              ],
+            ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('Add to Cart'),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('Add to Watch List'),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
