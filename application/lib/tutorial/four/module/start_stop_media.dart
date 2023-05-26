efd7b638d04507404a4e2d3f818e797fadab9c4f
Shreshth Saharan <ssah0022@student.monash.edu>
2023-05-26 14:03:56 +1000

    Should be working now

--- application/lib/tutorial/four/module/start_stop_media.dart
+++ application/lib/tutorial/four/module/start_stop_media.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 
 class StartStopMedia extends StatefulWidget {
   const StartStopMedia({super.key});
@@ -11,13 +12,23 @@ class StartStopMedia extends StatefulWidget {
 class _StartStopMediaState extends State<StartStopMedia> {
   late VideoPlayerController _controller;
   late Future<void> _initialiseVideoPlayerFuture;
+  bool _spokenIntro = false;
+  bool _hasPlayed = false;
+  bool _hasPaused = false;
+  final FlutterTts tts = FlutterTts();
+
+  startStopMediaState() async {
+    tts.setLanguage("en-US");
+    tts.setSpeechRate(0.5);
+    tts.setVolume(1.0);
+  }
 
   @override
   void initState() {
     super.initState();
 
-    _controller = VideoPlayerController.network(
-        'https://www.youtube.com/watch?v=jNQXAC9IVRw');
+    _controller =
+        VideoPlayerController.asset('/application/assets/video_test.mp4');
     _initialiseVideoPlayerFuture = _controller.initialize();
   }
 
@@ -27,11 +38,35 @@ class _StartStopMediaState extends State<StartStopMedia> {
     super.dispose();
   }
 
+  Future<void> _speakIntro() async {
+    await tts.speak(
+        "In this tutorial, you will be learning how to start and stop a video. To start, double tap your screen to bring up the media controller and find the play button. Dobule tap to start playing the video");
+  }
+
+  void _speakPause() {
+    tts.stop();
+    tts.speak(
+        "Great job! You have played the video. Now we will try to pause the video. To pause the video, bring up the media controller and find the pause button. Double tap on the button to pause the video.");
+  }
+
+  Future<void> _speakOutro() async {
+    tts.stop();
+    tts.speak(
+        "Great job! You have paused the video. Congratulations on completing this lesson on how to start and stop a video. Sending you back to the lesson page.");
+    await tts.awaitSpeakCompletion(true);
+  }
+
   @override
   Widget build(BuildContext context) {
+    if (!_spokenIntro) {
+      _speakIntro();
+      _spokenIntro = true;
+    }
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Me at the zoo'),
+        automaticallyImplyLeading: false,
+        title: const Text('Start and Stop Media Module'),
       ),
       body: FutureBuilder(
         future: _initialiseVideoPlayerFuture,
@@ -53,8 +88,15 @@ class _StartStopMediaState extends State<StartStopMedia> {
           setState(() {
             if (_controller.value.isPlaying) {
               _controller.pause();
+              if ((_hasPlayed == true) & (_hasPaused == false)) {
+                _hasPaused = true;
+                _speakOutro();
+                Navigator.pop(context);
+              }
             } else {
               _controller.play();
+              _speakPause();
+              _hasPlayed = true;
             }
           });
         },
