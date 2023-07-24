9ca56374c3f7b3f48c399602b02750408dd8371e
byebrid <lexlmgallon@gmail.com>
2023-07-25 09:49:48 +1000

    Fix text formatting in tutorial 2 adjust_slider
    - Note this also fixes long pauses in screen reader's speech due to all
      the spaces that were there before.

--- application/lib/tutorial/two/module/adjust_slider.dart
+++ application/lib/tutorial/two/module/adjust_slider.dart
@@ -3,11 +3,11 @@ import 'package:flutter/semantics.dart';
 import 'dart:io' show Platform;
 
 class AdjustSliderState extends State<AdjustSlider> {
-  @override
   double _currentValue = 0;
   int stage = 1;
   String displayText = "";
 
+  @override
   Widget build(BuildContext context) {
     if (stage == 1 && _currentValue == 100) {
       // Finished first part
@@ -29,7 +29,8 @@ class AdjustSliderState extends State<AdjustSlider> {
         body: Center(
             child: Column(children: [
           Text(displayText),
-          Text(_currentValue.toStringAsFixed(0), style: TextStyle(fontSize: 30)),
+          Text(_currentValue.toStringAsFixed(0),
+              style: const TextStyle(fontSize: 30)),
           Slider(
               value: _currentValue,
               onChanged: (value) {
@@ -43,20 +44,14 @@ class AdjustSliderState extends State<AdjustSlider> {
 
   String instructions(stage) {
     if (stage == 1) {
-      return """Welcome, in this module you'll learn how to  increase and decrease slider value. A slider is a track that
-          contains values between a minimum and a maximum. Sliders are often used to adjust times. For example: song times
-          or video times. You can also adjust this phone's volume using a slider. To start you will try and increase a slider's
-          value. To increase the value, swipe up. To continue, increase the slider value to 100%""";
+      return "Welcome, in this module you'll learn how to  increase and decrease slider value. A slider is a track that contains values between a minimum and a maximum. Sliders are often used to adjust times. For example: song times or video times. You can also adjust this phone's volume using a slider. To start you will try and increase a slider's value. To increase the value, swipe up. To continue, increase the slider value to 100%";
     } else if (stage == 2) {
-      return """Now we will try and move the slider back down. To do this swipe down with one finger to reduce the value.
-                  Continue until the value is back to 50%""";
+      return "Now we will try and move the slider back down. To do this swipe down with one finger to reduce the value. Continue until the value is back to 50%";
     } else if (stage == 3) {
-      return """An alternative way of changing the volume is to double tap and hold the slider. From here you
-                  can swipe left and right to adjust it in that direction. Try and go back to 100%""";
-    } //  Stage == 3
-    else {
+      return "An alternative way of changing the volume is to double tap and hold the slider. From here you can swipe left and right to adjust it in that direction. Try and go back to 100%";
+    } else {
       //stage == 4
-      return """Nice job! Use the back button to return home""";
+      return "Nice job! Use the back button to return home";
     }
   }
 }
@@ -64,7 +59,6 @@ class AdjustSliderState extends State<AdjustSlider> {
 class AdjustSlider extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    // TODO: implement createState
     return AdjustSliderState();
   }
 }
