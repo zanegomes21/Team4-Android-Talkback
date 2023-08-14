8f0f9b8fd862e82404c16ad0556d155985870528
haotongwang <nicholas.wang.h@gmail.com>
2023-08-14 12:56:47 +1000

    Add examples of usage and documentation

--- application/lib/resources/string/base.dart
+++ application/lib/resources/string/base.dart
@@ -1,13 +1,15 @@
+/// Supported languages across the application
 enum Language {
   english,
-  french;
+  french,
+  spanish;
 }
 
-/// Base class for any String resources
+/// Wrapper for resources that are language dependent.
 ///
 /// Allows conversion between different implementations of
-/// String resources, such as for different languages.
-abstract class LanguageSupport<T> {
+/// resources for different languages.
+class LanguageSupport<T> {
   static Language _language = Language.english;
 
   static void _setLanguage(Language language) {
@@ -18,41 +20,65 @@ abstract class LanguageSupport<T> {
     return LanguageSupport._language;
   }
 
-  // final T _instance;
+  final Map<Language, T> _instances;
+  final T _default;
+
+  const LanguageSupport(this._instances, this._default);
+
+  T getInstance() {
+    return _instances[LanguageSupport.getLanguage()] ?? this._default;
+  }
 }
 
-class _Example extends LanguageSupport<_Example> {
+/// Examples
+
+/// Instantiating an instance for a class of strings
+const LanguageSupport<_Example> _exampleStrings =
+    LanguageSupport(_Example.instances, _Example.english2);
+
+/// Class used as an object enum for strings
+class _Example {
   /// These can be created from a formatted file
-  static final _Example _english = _Example._internal(
+  static const _Example english = _Example._internal(
     "First",
     "Second",
     "Third",
   );
-  static final _Example _default = _Example._internal(
+  static const _Example english2 = _Example._internal(
     "One",
     "Two",
     "Three",
   );
+  static const _Example french = _Example._internal(
+    "1",
+    "2",
+    "3",
+  );
+
+  /// Instances of this class that can be switched between
+  static const Map<Language, _Example> instances = {
+    Language.english: _Example.english,
+    Language.french: _Example.french,
+  };
 
   final String first;
   final String second;
   final String third;
 
-  _Example._internal(this.first, this.second, this.third);
-
-  factory _Example() {
-    switch (LanguageSupport.getLanguage()) {
-      case Language.english:
-        return _english;
-      default:
-        return _default;
-    }
-  }
+  const _Example._internal(this.first, this.second, this.third);
 }
 
 void main() {
   LanguageSupport._setLanguage(Language.english);
-  print(_Example().first);
-  print(_Example().second);
-  print(_Example().third);
+  print(_exampleStrings.getInstance().first);
+  print(_exampleStrings.getInstance().second);
+  print(_exampleStrings.getInstance().third);
+  LanguageSupport._setLanguage(Language.french);
+  print(_exampleStrings.getInstance().first);
+  print(_exampleStrings.getInstance().second);
+  print(_exampleStrings.getInstance().third);
+  LanguageSupport._setLanguage(Language.spanish);
+  print(_exampleStrings.getInstance().first);
+  print(_exampleStrings.getInstance().second);
+  print(_exampleStrings.getInstance().third);
 }
