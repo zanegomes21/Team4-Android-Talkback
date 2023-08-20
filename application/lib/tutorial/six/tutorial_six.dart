05fe91a0fd8b8f6f882efa2d2365175701aaa428
byebrid <lexlmgallon@gmail.com>
2023-08-20 11:34:09 +1000

    Refactor tutorials to use TutorialMenu class
    - Add `common/tutorial.dart`. This contains `TutorialMenu`, which represents one of our Tutorial's menus, which itself will contain some `TutorialMenuButton`s, one for each module/challenge. This massively simplifies our tutorial code since we had copy-pasted everywhere.
    - Update tutorials 2, 3, 4, 6 to use these template classes.
    - Remove `tutorial_main` since it was actually jsut doubling up on home page.
    - Remove needless doc comments from template flutter app.
    - Make HomePage stateless since it didn't need state.

--- application/lib/tutorial/six/tutorial_six.dart
+++ application/lib/tutorial/six/tutorial_six.dart
@@ -1,43 +1,9 @@
-import 'package:application/tutorial/six/module/type_with_virtual_keyboard.dart';
-import 'package:flutter/material.dart';
+import 'package:application/common/tutorial.dart';
 import 'package:application/tutorial/six/module/chat_view.dart';
+import 'package:application/tutorial/six/module/type_with_virtual_keyboard.dart';
 
-class TutorialSix extends StatelessWidget {
-  const TutorialSix({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    // scaffold page
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Tutorial 6"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const TypeWithKeyboard()),
-                );
-              },
-              child: const Text("Typing with a virtual keyboard"),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => ChatView()),
-                );
-              },
-              child: Text("End of Lesson challenges"),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+TutorialMenu tutorialSix = TutorialMenu(title: 'Tutorial 6', moduleButtons: [
+  const TutorialMenuButton(
+      title: 'Typing with a virtual keyboard', module: TypeWithKeyboard()),
+  TutorialMenuButton(title: 'Challenge', module: ChatView())
+]);
--- application/lib/tutorial/two/tutorial_two.dart
+++ application/lib/tutorial/two/tutorial_two.dart
@@ -1,74 +1,14 @@
+import 'package:application/common/tutorial.dart';
 import 'package:application/tutorial/two/module/adjust_slider.dart';
+import 'package:application/tutorial/two/module/explore_menu.dart';
 import 'package:application/tutorial/two/module/go_back.dart';
 import 'package:application/tutorial/two/module/scroll.dart';
-import 'package:application/tutorial/two/module/explore_menu.dart';
-import 'package:application/tutorial/two/practical/homepage.dart';
-import 'package:flutter/material.dart';
 
-class TutorialTwo extends StatelessWidget {
-  const TutorialTwo({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    // scaffold page
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Tutorial 2"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const GoBack()),
-                );
-              },
-              child: const Text("Go Back Module"),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const VerticalScrollSubmodule()),
-                );
-              },
-              child: const Text("Scrolling Module"),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const ExploreMenuPage()),
-                );
-              },
-              child: const Text("Explore Menu Module"),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const AdjustSlider()),
-                );
-              },
-              child: const Text("Adjust slide module"),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const HomePage()),
-                );
-              },
-              child: const Text("Practical Application"),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+const TutorialMenu tutorialTwo =
+    TutorialMenu(title: 'Tutorial 2', moduleButtons: [
+  TutorialMenuButton(title: 'Go back', module: GoBack()),
+  TutorialMenuButton(title: 'Scrolling', module: VerticalScrollSubmodule()),
+  TutorialMenuButton(title: 'Explore menu', module: ExploreMenuPage()),
+  TutorialMenuButton(title: 'Adjust slider', module: AdjustSlider()),
+  // TutorialMenuButton(title: 'Practical application', module: null),
+]);
