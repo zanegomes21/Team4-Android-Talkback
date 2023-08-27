995477451b88e7193a284da390b24a7cb026c7e4
DanH315 <62536655+DanH315@users.noreply.github.com>
2023-08-28 01:45:26 +1000

    Sandbox mode update for multi finger swiping. Issue: immediately detects as a single finger swipe if both fingers do not leave screen simultaneously

--- application/lib/sandbox_mode.dart
+++ application/lib/sandbox_mode.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class SandBox extends StatefulWidget {
@@ -10,26 +12,58 @@ class SandBox extends StatefulWidget {
 }
 
 class GesturesState extends State<SandBox> {
+  // Timer? timer;
+
   void doubleTap() => {
         setState(() => {gestureText = "Double tap"})
       };
   void longPress() => {
         setState(() => {gestureText = "Long press"})
       };
-  void verticalSwipe(details) => {
-        setState(() => {gestureText = "Vertical swipe"})
-      };
-  void horizontalSwipe(details) => {
-        setState(() => {gestureText = "Horizontal swipe"})
+  void handleSwipe(ScaleUpdateDetails details) => {
+        if (details.pointerCount == 1)
+          if (details.focalPointDelta.dx.abs() >
+              details.focalPointDelta.dy.abs())
+            {
+              setState(() => {gestureText = "Horizontal swipe"})
+            }
+          else if (details.focalPointDelta.dx.abs() <=
+              details.focalPointDelta.dy.abs())
+            {
+              setState(() => {gestureText = "Vertical swipe"})
+            },
+        if (details.pointerCount == 2)
+          if (details.focalPointDelta.dx.abs() >
+              details.focalPointDelta.dy.abs())
+            {
+              setState(() => {gestureText = "Two finger horizontal swipe"}),
+              // timer = Timer(const Duration(milliseconds: 500), () {})
+            }
+          else if (details.focalPointDelta.dx.abs() <=
+              details.focalPointDelta.dy.abs())
+            {
+              setState(() => {gestureText = "Two finger vertical swipe"}),
+              // timer = Timer(const Duration(milliseconds: 500), () {})
+            },
+        if (details.pointerCount == 3)
+          if (details.focalPointDelta.dx.abs() >
+              details.focalPointDelta.dy.abs())
+            {
+              setState(() => {gestureText = "Three finger horizontal swipe"})
+            }
+          else if (details.focalPointDelta.dx.abs() <=
+              details.focalPointDelta.dy.abs())
+            {
+              setState(() => {gestureText = "Three finger vertical swipe"})
+            }
       };
   String gestureText = 'No gestures yet';
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
+      onScaleUpdate: handleSwipe,
       onDoubleTap: doubleTap,
       onLongPress: longPress,
-      onVerticalDragEnd: verticalSwipe,
-      onHorizontalDragEnd: horizontalSwipe,
       child: Scaffold(
           appBar: AppBar(title: const Text("SandBox")),
           body: Center(
