8786dfa7b5d6412bac264e2dc25bd6c78f5303a8
DanH315 <62536655+DanH315@users.noreply.github.com>
2023-08-27 18:31:22 +1000

    module 1 complete. module 2 in progress

--- application/lib/tutorial/three/module/adjust_reading_controls.dart
+++ application/lib/tutorial/three/module/adjust_reading_controls.dart
@@ -1,34 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:application/common/instruction_card.dart';
 
-class TextBox extends StatelessWidget {
-  const TextBox({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: const TextField(
-        decoration: InputDecoration(
-          border: OutlineInputBorder(),
-          labelText:
-              'For modules in this lesson, you\'ll need to use horizontal swipes to navigate',
-        ),
-      ),
-    );
-  }
+class AdjustReadingControls extends InstructionsCard {
+  const AdjustReadingControls({super.key})
+      : super(
+            instruction:
+                'For modules in this lesson, you will need to use horizontal swipes to navigate. This module covers how to AdjustReading the different reading modes. Swipe with 3 fingers horizontally on the screen to change reading modes.');
+//add a method to check for a 3 finger horizontal swipe
 }
 
-class AdjustReadingControls extends StatelessWidget {
-  const AdjustReadingControls({Key? key}) : super(key: key);
+class AdjustReadingControlsTypes extends InstructionsCard {
+  const AdjustReadingControlsTypes({super.key})
+      : super(
+            instruction:
+                'There are reading and navigation modes. Reading modes help you read consecutive characters, words, and paragraphs. Navigation modes help you move around the screen better, by jumping from button to button. Swipe horizontally with 1 finger to go to the next paragraph');
+}
+
+class AdjustReadingControlsFinal extends InstructionsCard {
+  const AdjustReadingControlsFinal({super.key})
+      : super(
+            instruction:
+                'That\'s all there is to the different reading controls. Go through all of them to understand them. Navigate to the bottom of the page and press the button to complete the module');
+}
+
+class AdjustReadingReadingControls extends StatelessWidget {
+  const AdjustReadingReadingControls({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Adjusting reading controls'),
+        title: const Text('Adjust reading controls'),
       ),
-      body: const Center(
-        child: TextBox(),
-      ),
+      body: Center(
+          child: Column(children: [
+        const AdjustReadingControls(),
+        const AdjustReadingControlsTypes(),
+        const AdjustReadingControlsFinal(),
+        ElevatedButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text('Finish Module'))
+      ])),
     );
   }
 }
--- application/lib/tutorial/three/module/jump_text.dart
+++ application/lib/tutorial/three/module/jump_text.dart
@@ -1,1 +1,35 @@
+import 'package:application/common/instruction_card.dart';
+import 'package:flutter/material.dart';
 
+//page1
+class JumpText1 extends StatelessWidget {
+  const JumpText1({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+            child: Column(children: <Widget>[
+      GestureDetector(
+        onDoubleTap: () {
+          //navigate to JumpText2
+        },
+      ),
+      const InstructionsCard(
+        instruction: 'instruction',
+      )
+    ])));
+  }
+}
+
+// //page2
+// class JumpText2 extends StatelessWidget {}
+
+// //page3
+// class JumpText3 extends StatelessWidget {}
+
+// //page4
+// class JumpText4 extends StatelessWidget {}
+
+// //page5
+// class JumpText5 extends StatelessWidget {}
